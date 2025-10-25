fc84ee23345e47fb0b705757edcfc432f8a67600
marmot <arctic_marmot@outlook.com>
2025-10-25 17:15:06 +0800

    feat: init the keypair module

--- account/src/error.rs
+++ account/src/error.rs
@@ -2,8 +2,8 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum AccountError {
-    #[error("bech32 error: {0}")]
-    Bech32(#[from] bech32::Error),
-    #[error("ed25519 signature error")]
-    Signature
+    #[error("invalid ed25519 verifying key bytes")]
+    InvalidVerifyingKey(#[source] ed25519_dalek::SignatureError),
+    #[error("ed25519 signature verification failed")]
+    SignatureVerifyingError(#[source] ed25519_dalek::SignatureError),
 }
--- account/src/keypair.rs
+++ account/src/keypair.rs
@@ -1,27 +1,74 @@
 use ed25519_dalek::{Signer, Verifier};
 use ed25519_dalek::{SigningKey, VerifyingKey, Signature};
 use rand_core::OsRng;
+use crate::error::AccountError;
 
-pub type SignatureBytes = [u8; 64];
+
+pub type Result<T> = std::result::Result<T, AccountError>;
+pub type SigBytes = [u8; 64];
+pub type SigningKeyBytes = [u8; 32];
+pub type VerifyingKeyBytes = [u8; 32];
+// 账户私钥
+#[derive(Debug)]
+pub struct AccountSigningKey(SigningKey);
+
+// 账户公钥
+#[derive(Debug)]
+pub struct AccountVerifyingKey(VerifyingKey);
+
+impl AccountSigningKey {
+    pub fn from_bytes(signing_key_bytes: &SigningKeyBytes) -> Self {
+        Self(SigningKey::from_bytes(signing_key_bytes))
+    }
+
+    pub fn sign(&self, msg: &[u8]) -> Signature {
+        self.0.sign(msg)
+    }
+
+    pub fn verifying_key(&self) -> AccountVerifyingKey {
+        AccountVerifyingKey(self.0.verifying_key())
+    }
+}
+
+impl AccountVerifyingKey {
+    pub fn from_bytes(verifying_key_bytes: &VerifyingKeyBytes) -> Result<Self> {
+        let verifying_key = VerifyingKey::from_bytes(verifying_key_bytes)
+            .map_err(AccountError::InvalidVerifyingKey)?;
+        Ok(Self(verifying_key))
+    }
+
+    pub fn verify(&self, msg: &[u8], sig: &Signature) -> Result<()> {
+        self.0.verify(msg, sig).map_err(AccountError::SignatureVerifyingError)?;
+        Ok(())
+    }
+
+    pub fn is_valid_sig(&self, msg: &[u8], sig: &Signature) -> bool {
+        self.verify(msg, sig).is_ok()
+    }
+}
 
 #[derive(Debug)]
 pub struct Keypair {
-    pub secret: SigningKey,
-    pub public: VerifyingKey
+    pub signing_key: AccountSigningKey,
+    pub verifying_key: AccountVerifyingKey
 }
 
 impl Keypair {
     // random generate keypair
-    pub fn new() -> Self {
+    pub fn generate() -> Self {
         let mut csprng = OsRng;
-        let secret = SigningKey::generate(&mut csprng);
-        let public = secret.verifying_key();
+        let signing_key = AccountSigningKey(SigningKey::generate(&mut csprng));
+        let verifying_key = signing_key.verifying_key();
         Self {
-            secret,
-            public
+            signing_key,
+            verifying_key
         }
     }
 }
+
+
+
+
 
 #[cfg(test)]
 mod tests {
@@ -29,7 +76,21 @@ mod tests {
 
     #[test]
     fn test_keypair_generate() {
-        let kp = Keypair::new();
-        println!("{:?}", kp);
+        let keypair = Keypair::generate();
+        println!("{:?}", keypair);
+    }
+
+    #[test]
+    fn test_sign_and_verify() {
+        let keypair = Keypair::generate();
+        let sk = keypair.signing_key;
+        let pk = keypair.verifying_key;
+        let msg = [100, 150, 200];
+        let sig = sk.sign(&msg);
+        println!("{:?}", sig);
+        let result = pk.verify(&msg, &sig);
+        println!("{:?}", result);
+        let is_valid_sig = pk.is_valid_sig(&msg, &sig);
+        assert_eq!(is_valid_sig, true);
     }
 }
